960d1ebc0e2f39896c0020f1f4d95f4c974d3c40
spindev79 <161652842+spindev79@users.noreply.github.com>
2025-02-16 12:34:04 +1300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -5,8 +5,9 @@ public class helloworld {
 	public static void main(final String[] args) {
 		
 		
-		System.out.print("Hello world");
-		
+		System.out.print("Hello world")
+			; 
+		System.out.print("This is an updated version");
 		
 	}
 
