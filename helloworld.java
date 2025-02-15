5be8b1f580b0b9662c3d204f65face9268878c47
spindev79 <161652842+spindev79@users.noreply.github.com>
2025-02-16 11:09:50 +1300

    Final update
    Contains final updates to project

---
+++ helloworld.java
@@ -0,0 +1,20 @@
+package assessment2task9;
+
+public class helloworld {
+	
+	public static void main(final String[] args) {
+		
+		
+		System.out.print("Hello world");
+		
+		System.out.print("This is an updated version with additional text");
+		
+		System.out.print("This is the final update ready for release");
+		
+	}
+
+
+	
+	
+	
+}
