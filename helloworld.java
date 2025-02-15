14657d0c1cc387cddeaa124d6356d01ac3a2689c
spindev79 <161652842+spindev79@users.noreply.github.com>
2025-02-16 11:08:30 +1300

    Update
    This is an updated version with additional text

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+package assessment2task9;
+
+public class helloworld {
+	
+	public static void main(final String[] args) {
+		
+		
+		System.out.print("Hello world");
+		
+		System.out.print("This is an updated version with additional text");
+		
+		
+	}
+
+
+	
+	
+	
+}
